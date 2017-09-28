c01119a26241bea68e41dd44f6e0c5d89202b4cc
James Osborn and Scott Reichert <scott@>
2017-09-28 14:13:21 -0700

    rename output variables

--- README.md
+++ README.md
@@ -20,14 +20,17 @@ The program will add round score to total score when the player presses the hold
   Example Input: Player presses the hold button
   Example Output: Score is held and displayed into the outputs
 
-*The program will revert to the hold score if the player rolls a 1
+The program will revert to the hold score if the player rolls a 1
   Example Input: Player rolls a 1
-  Example Output: Value is returned to the last hold score.*
+  Example Output: Value is returned to the last hold score.
 
 The program will terminate when one player reach a number >= 100
   Example Input: Players score is 100
   Example Output: Player is prompted with a "you win" statement
 
+The program will accept input from a 2nd player, adding total score to a new output.
+  Example Input: Player 2 presses their Hold button
+  Example Output: Player 2 total score is held in a output separate from Player 1.
 ***
 
 The program will hold the player 1 score and switch to player 2 scoring after clicking hold
--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,53 @@
-function Player(roundScore, totalScore) {
+function Player1(roundScore, totalScore) {
   this.roundScore = 0;
   this.totalScore = 0;
-
 }
-// Player.prototype.to randomize first roll
-Player.prototype.dice = function () {
+Player1.prototype.dice = function () {
   var roll1= Math.floor(Math.random() * (6 - 1 + 1)) + 1;
   this.roundScore += roll1;
   return roll1;
 }
-Player.prototype.bust = function (roundRoll) {
+Player1.prototype.bust = function (roundRoll) {
+  if (roundRoll === 1) {
+    this.roundScore = 0;
+    }
+}
+
+function Player2(roundScore, totalScore) {
+  this.roundScore = 0;
+  this.totalScore = 0;
+}
+Player2.prototype.dice = function () {
+  var roll1= Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+  this.roundScore += roll1;
+  return roll1;
+}
+Player2.prototype.bust = function (roundRoll) {
   if (roundRoll === 1) {
     this.roundScore = 0;
     }
   }
 $(document).ready(function() {
-  var inputPlayer = new Player ();
+  var inputPlayer = new Player1 ();
+  var inputPlayer2 = new Player2 ();
   // Submit Button
   $("#button1").click(function(event) {
-  event.preventDefault();
-  var diceRoll = inputPlayer.dice();
-  inputPlayer.bust(diceRoll);
-  console.log(diceRoll);
-  console.log(inputPlayer.roundScore);
-  console.log(inputPlayer.totalScore);
-  $("#output2").text("Round Score: "+ inputPlayer.roundScore);
-  $("#output").text("Last Roll: " + diceRoll);
-  });
+    event.preventDefault();
+    var diceRoll = inputPlayer.dice();
+    inputPlayer.bust(diceRoll);
+    $("#outputLastRoll1").text("Last Roll: " + diceRoll);
+    $("#outputRoundScore1").text("Round Score: "+ inputPlayer.roundScore);
+    });
   // Hold button
   $("#button2").click(function(event) {
     event.preventDefault();
     inputPlayer.totalScore = inputPlayer.totalScore + inputPlayer.roundScore;
     inputPlayer.roundScore = 0;
-    $("#output2").text("Round Score: "+ inputPlayer.roundScore);
-    $("#output3").text("Total: " + inputPlayer.totalScore);
-    console.log(inputPlayer.totalScore);
+    $("#outputRoundScore1").text("Round Score: "+ inputPlayer.roundScore);
+    $("#outputTotalScore1").text("Total: " + inputPlayer.totalScore);
     if (inputPlayer.totalScore >= 100) {
       alert("You are winner!")
-    }
+      }
 });
 });
 // Make a new function for other buttons
